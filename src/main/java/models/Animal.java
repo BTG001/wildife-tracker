45d6e9b42de1e355c03c1f5709b82d31a28c1a33
BTG001 <brunokipkorir2@gmail.com>
2020-07-23 20:57:06 +0300

    Test that animal age can be retrieved

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -9,5 +9,26 @@ import java.util.List;
 import java.util.Objects;
 
 public class Animal implements AnimalInterface{
+    private int animal_id;
+    private String animal_name;
+    private String animal_health;
+    private String animal_age;
+    private int id;
 
+    public Animal(int animal_id, String animal_name){
+        this.animal_id = animal_id;
+        this.animal_name = animal_name;
+        this.id = 0;
+    }
+    public static Animal setUpNewAnimal() {
+        return new Animal(1, "Koala Bear");
+    }
+
+    public int getAnimal_id() {
+        return animal_id;
+    }
+
+    public String getAnimal_age() {
+        return animal_age;
+    }
 }
--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -4,4 +4,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+
+    @Test
+    public void getAnimal_age() {
+        Animal newAnimal = Animal.setUpNewAnimal();
+        assertEquals("young",newAnimal.getAnimal_age());
+    }
 }
