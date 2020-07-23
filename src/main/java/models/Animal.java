652dba1962efd72c6fe70ce9d69c87f9feb41e36
BTG001 <brunokipkorir2@gmail.com>
2020-07-23 20:41:16 +0300

    Animal class implements the AnimalInterface

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,4 +1,13 @@
 package models;
 
-public class Animal {
+import interfaces.AnimalInterface;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Animal implements AnimalInterface{
+
 }
