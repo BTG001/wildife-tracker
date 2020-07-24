4dc821fc42af20bcd0f3762375eefe0c92ae19dc
BTG001 <brunokipkorir2@gmail.com>
2020-07-24 16:31:03 +0300

    Create environment for Heroku deployment

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,28 +6,23 @@ import models.Sighting;
 import org.sql2o.Connection;
 import spark.ModelAndView;
 import spark.template.handlebars.*;
-
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 
     public static void main(String[] args) {
-        ProcessBuilder process = new ProcessBuilder();
-        Integer port;
 
-        // This tells our app that if Heroku sets a port for us, we need to use that port.
-        // Otherwise, if they do not, continue using port 4567.
-
-        if (process.environment().get("PORT") != null) {
-            port = Integer.parseInt(process.environment().get("PORT"));
-        } else {
-            port = 4567;
-        }
-
-        port(port);
-
+        port(getHerokuAssignedPort());
         Connection conn;
         staticFileLocation("/public");
 
