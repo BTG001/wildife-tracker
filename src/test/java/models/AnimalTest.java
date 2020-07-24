3a2ffcb7755f737cea2c93bddd7eea92178f200a
BTG001 <brunokipkorir2@gmail.com>
2020-07-24 13:54:36 +0300

    Create basic layout of the page

--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -1,9 +1,13 @@
 package models;
 
+import org.junit.Rule;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
 
     @Test
     public void getAnimal_age() {
