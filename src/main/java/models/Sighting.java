bcb45f91cec23f09adea94a5081690c7edc91120
BTG001 <brunokipkorir2@gmail.com>
2020-07-24 12:55:22 +0300

    Create constructor for class Sighting and create an instance of the class and named it setUpSighting

--- src/main/java/models/Sighting.java
+++ src/main/java/models/Sighting.java
@@ -1,4 +1,29 @@
 package models;
 
+import org.sql2o.Connection;
+
+import java.sql.ResultSet;
+import java.sql.Time;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Objects;
+
 public class Sighting {
+    private String animal_location;
+    private String ranger_name;
+    private int animal_id;
+    private int id;
+    private DateTimeFormatter time;
+//    private static ArrayList<Animal> allAnimals = new ArrayList<>();
+
+    public Sighting(int animalId,String location, String rangerName){
+        this.animal_location = location;
+        this.ranger_name = rangerName;
+        this.animal_id = animalId;
+        this.id = 0;
+    }
+    public static Sighting setUpSighting() {
+        return new Sighting(1, "Zone-A", "Cliff");
+    }
+
 }
--- src/test/java/models/SightingTest.java
+++ src/test/java/models/SightingTest.java
@@ -1,4 +1,8 @@
 package models;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class SightingTest {
+
 }
